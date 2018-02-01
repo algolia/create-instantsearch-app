ddc7aaa91f1ce5b6b5eb920a34558b051a3d1776
François Chalifour <francois.chalifour@gmail.com>
2018-02-01 11:07:53 +0100

    feat(version): fetch latest version all the time

--- bin/create-instantsearch-app.js
+++ bin/create-instantsearch-app.js
@@ -5,108 +5,81 @@ const path = require('path');
 const program = require('commander');
 const prompt = require('prompt');
 const colors = require('colors');
-const algoliasearch = require('algoliasearch');
 
-const version = require('../package.json').version;
-const createProject = require('../lib/createProject.js');
-const { algolia } = require('../config');
+const { version } = require('../package.json');
+const { fetchLatestVersion } = require('../lib/fetchVersions');
+const createProject = require('../lib/createProject');
 
-const client = algoliasearch(algolia.appId, algolia.apiKey);
-const index = client.initIndex(algolia.indexName);
+let opts = {};
+let targetFolderName;
 
-(async function() {
-  let latestVersion;
-  let allVersions;
+program
+  .version(version)
+  .arguments('<destination_folder>')
+  .option('--app-id <appId>', 'The application ID')
+  .option('--api-key <apiKey>', 'The Algolia search API key')
+  .option('--index-name <indexName>', 'The main index of your search')
+  .option(
+    '--main-attribute <attributeName>',
+    'The main searchable attribute of your index'
+  )
+  .action(function(dest, options) {
+    opts = options;
+    targetFolderName = dest;
+  })
+  .parse(process.argv);
 
-  try {
-    const content = await index.search('instantsearch.js');
-    const library = content.hits[0];
-    latestVersion = library.version;
-    allVersions = Object.keys(library.versions);
-  } catch (err) {
-    // `AlgoliaSearchNetworkError` can occur if offline.
-    // Defaults to the latest major version.
-    latestVersion = '2';
-    allVersions = [latestVersion];
-  }
-
-  let opts = {};
-  let targetFolderName;
-
-  program
-    .version(version)
-    .arguments('<destination_folder>')
-    .option('--app-id <appId>', 'The application ID')
-    .option('--api-key <apiKey>', 'The Algolia search API key')
-    .option('--index-name <indexName>', 'The main index of your search')
-    .option(
-      '--main-attribute <attributeName>',
-      'The main searchable attribute of your index'
-    )
-    .option('--lib-version <libVersion>', 'The InstantSearch.js library version')
-    .action(function(dest, options) {
-      opts = options;
-      targetFolderName = dest;
-    })
-    .parse(process.argv);
-
-  if (!targetFolderName) {
-    console.log(
-      'The folder name for the new instantsearch project was not provided 😲'
-        .red
-    );
-    program.help();
-  }
-
+if (!targetFolderName) {
   console.log(
-    `Creating your new instantsearch app: ${targetFolderName.bold}`.green
+    'The folder name for the new InstantSearch project was not provided 😲'.red
   );
+  program.help();
+}
 
-  let prompts = [
-    {
-      name: 'appId',
-      description: 'Application ID'.blue,
-      required: true,
-    },
-    {
-      name: 'apiKey',
-      description: 'Search API key'.blue,
-      required: true,
-    },
-    {
-      name: 'indexName',
-      description: 'Index name'.blue,
-      required: true,
-    },
-    {
-      name: 'libVersion',
-      description: `InstantSearch version`.blue,
-      default: latestVersion,
-      required: false,
-      conform: value => allVersions.includes(value),
-      message:
-        "This version of InstantSearch.js doesn't exist (see https://yarnpkg.com/en/package/instantsearch.js)",
-    },
-    {
-      name: 'attributeName',
-      description: 'Main searchable attribute'.blue,
-      required: false,
-    },
-  ];
+console.log(
+  `Creating your new InstantSearch app: ${targetFolderName.bold}`.green
+);
 
-  prompt.message = '';
-  prompt.override = opts;
+let prompts = [
+  {
+    name: 'appId',
+    description: 'Application ID'.blue,
+    required: true,
+  },
+  {
+    name: 'apiKey',
+    description: 'Search API key'.blue,
+    required: true,
+  },
+  {
+    name: 'indexName',
+    description: 'Index name'.blue,
+    required: true,
+  },
+  {
+    name: 'attributeName',
+    description: 'Main searchable attribute'.blue,
+    required: false,
+  },
+];
 
-  prompt.start();
-  prompt.get(prompts, function(err, config) {
-    if (err) {
-      console.log('\nProject creation cancelled 😢'.red);
-      process.exit(0);
-    } else {
-      config.name = targetFolderName;
-      config.targetFolderName = path.join(process.cwd(), targetFolderName);
-      createProject(config);
-      console.log('Project successfully created 🚀'.green.bold);
-    }
-  });
-})();
+prompt.message = '';
+prompt.override = opts;
+
+prompt.start();
+prompt.get(prompts, async (err, config) => {
+  if (err) {
+    console.log('\nProject creation cancelled 😢'.red);
+    process.exit(0);
+  }
+
+  config.name = targetFolderName;
+  config.targetFolderName = path.join(process.cwd(), targetFolderName);
+
+  const libVersion = await fetchLatestVersion();
+  const configWithVersion = Object.assign({}, config, { libVersion });
+
+  createProject(configWithVersion);
+
+  console.log('Project successfully created 🚀'.green.bold);
+});
