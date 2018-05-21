6c21a6becdf6e786e91590ffa5fffb247c246e99
François Chalifour <francois.chalifour@gmail.com>
2018-05-21 16:07:31 +0200

    refactor(cli): Move CLI utils to `cli` folder

--- packages/shared/utils.js
+++ packages/shared/utils.js
@@ -42,46 +42,6 @@ function checkAppPath(path) {
   return true;
 }
 
-function camelCase(string) {
-  return string.replace(/-([a-z])/g, str => str[1].toUpperCase());
-}
-
-function getOptionsFromArguments(rawArgs) {
-  let argIndex = 0;
-
-  return rawArgs.reduce((allArgs, currentArg) => {
-    argIndex++;
-
-    if (!currentArg.startsWith('--') || currentArg.startsWith('--no-')) {
-      return allArgs;
-    }
-
-    const argumentName = camelCase(currentArg.split('--')[1]);
-    const argumentValue = rawArgs[argIndex];
-
-    return {
-      ...allArgs,
-      [argumentName]: argumentValue,
-    };
-  }, {});
-}
-
-function isQuestionAsked({ question, args }) {
-  for (const optionName in args) {
-    if (question.name === optionName) {
-      // Skip if the arg in the command is valid
-      if (question.validate && question.validate(args[optionName])) {
-        return false;
-      }
-    } else if (!question.validate) {
-      // Skip if the question is optional and not given in the command
-      return false;
-    }
-  }
-
-  return true;
-}
-
 function getLibraryName(name) {
   return name.toLowerCase().replace(/ /g, '-');
 }
@@ -120,10 +80,7 @@ async function fetchLibraryVersions(libraryName) {
 module.exports = {
   checkAppName,
   checkAppPath,
-  getOptionsFromArguments,
-  isQuestionAsked,
   isYarnAvailable,
-  camelCase,
   getTemplateName,
   getLibraryName,
   fetchLibraryVersions,
