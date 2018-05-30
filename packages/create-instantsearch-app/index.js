28b872ad150be178ba962640e1f70beb56f74ba4
François Chalifour <francois.chalifour@gmail.com>
2018-05-30 18:45:38 +0200

    feat(api): Default options and tasks to empty objects

--- packages/create-instantsearch-app/index.js
+++ packages/create-instantsearch-app/index.js
@@ -1,5 +1,9 @@
 const CreateInstantSearchApp = require('./CreateInstantSearchApp');
 
-module.exports = function createInstantSearchApp(path, config, tasks) {
+module.exports = function createInstantSearchApp(
+  path,
+  config = {},
+  tasks = {}
+) {
   return new CreateInstantSearchApp(path, config, tasks);
 };
