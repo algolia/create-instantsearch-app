e7fe595b8d2770febed97cf8c10f23055d4cabe1
François Chalifour <francois.chalifour@gmail.com>
2018-06-05 17:23:47 +0200

    feat(tasks): Support iOS

---
+++ packages/tasks/ios/teardown.js
@@ -0,0 +1,24 @@
+const chalk = require('chalk');
+
+module.exports = function teardown(config) {
+  if (!config.silent) {
+    try {
+      console.log();
+      console.log(
+        `🎉  Created ${chalk.bold.cyan(config.name)} at ${chalk.green(
+          config.path
+        )}.`
+      );
+      console.log();
+    } catch (err) {
+      console.log();
+      console.error(chalk.red('🛑  The app generation failed.'));
+      console.error(err);
+      console.log();
+
+      return Promise.reject(err);
+    }
+  }
+
+  return Promise.resolve();
+};
