b483446e580e7068b3208d47efadbad6ead0c04b
François Chalifour <francois.chalifour@gmail.com>
2018-07-19 14:31:25 +0200

    feat(template): Update templates

--- angular-instantsearch/src/app/app.component.ts
+++ angular-instantsearch/src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import * as algoliasearch from 'algoliasearch';
 
+const searchClient = algoliasearch(
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+);
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +14,6 @@ import * as algoliasearch from 'algoliasearch';
 export class AppComponent {
   config = {
     indexName: 'instant_search',
-    searchClient: algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76'),
+    searchClient,
   };
 }
--- react-instantsearch/src/App.js
+++ react-instantsearch/src/App.js
@@ -12,8 +12,8 @@ import PropTypes from 'prop-types';
 import './App.css';
 
 const searchClient = algoliasearch(
- 'latency',
- '6be0576ff61c053d5f9a3225e2a90f76'
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
 );
 
 class App extends Component {
