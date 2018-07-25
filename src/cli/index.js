d158fc60749c79326ea33586b4ad234f5d67219f
François Chalifour <francois.chalifour@gmail.com>
2018-07-25 13:39:41 +0200

    fix(cli): Update interface for "Attribute to display"

--- src/cli/index.js
+++ src/cli/index.js
@@ -179,7 +179,8 @@ const questions = [
   {
     type: 'list',
     name: 'mainAttribute',
-    message: 'Main searchable attribute – we will use it to generate a basic result template',
+    message: 'Attribute to display',
+    suffix: `\n  ${chalk.gray('Used to generate the default result template')}`,
     choices: async answers => [
       {
         name: 'None',
