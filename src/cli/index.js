8231e1889a27a8348718689b7d1c6b7fe8983d2e
François Triquet <francois.triquet@recast.ai>
2018-10-17 21:46:50 +0200

    ask app path if not given in command line

--- src/cli/index.js
+++ src/cli/index.js
@@ -26,7 +26,7 @@ let options = {};
 
 program
   .version(version, '-v, --version')
-  .arguments('<project-directory>')
+  .arguments('[project-directory]')
   .usage(`${chalk.green('<project-directory>')} [options]`)
   .option('--name <name>', 'The name of the application')
   .option('--app-id <appId>', 'The application ID')
@@ -50,188 +50,187 @@ program
   })
   .parse(process.argv);
 
-if (!appPath) {
-  console.log('Please specify the project directory:');
-  console.log();
-  console.log(
-    `  ${chalk.cyan('create-instantsearch-app')} ${chalk.green(
-      '<project-directory>'
-    )}`
-  );
-  console.log();
-  console.log('For example:');
-  console.log(
-    `  ${chalk.cyan('create-instantsearch-app')} ${chalk.green(
-      'my-instantsearch-app'
-    )}`
-  );
-  console.log();
-  console.log(
-    `Run ${chalk.cyan('create-instantsearch-app --help')} to see all options.`
-  );
-
-  process.exit(1);
-}
-
-const optionsFromArguments = getOptionsFromArguments(options.rawArgs);
-const appName = optionsFromArguments.name || path.basename(appPath);
-const attributesToDisplay = (optionsFromArguments.attributesToDisplay || '')
-  .split(',')
-  .filter(Boolean)
-  .map(x => x.trim());
-
-try {
-  checkAppPath(appPath);
-  checkAppName(appName);
-} catch (err) {
-  console.error(err.message);
-  console.log();
-
-  process.exit(1);
-}
-
-const questions = [
-  {
-    type: 'list',
-    pageSize: 10,
-    name: 'template',
-    message: 'InstantSearch template',
-    choices: () => {
-      const templatesByCategory = getTemplatesByCategory();
-
-      return Object.entries(templatesByCategory).reduce(
-        (templates, [category, values]) => [
-          ...templates,
-          new inquirer.Separator(category),
-          ...values,
-        ],
-        []
-      );
+const getAppPath = userPath => {
+  if (userPath) {
+    return Promise.resolve(userPath);
+  }
+  const askAppPath = [
+    {
+      type: 'input',
+      name: 'appPath',
+      message: 'Application directory',
     },
-    validate(input) {
-      return Boolean(input);
+  ];
+  return inquirer.prompt(askAppPath).then(r => r.appPath);
+};
+
+getAppPath(appPath).then(applicationPath => {
+  const optionsFromArguments = getOptionsFromArguments(options.rawArgs);
+  const appName = optionsFromArguments.name || path.basename(applicationPath);
+  const attributesToDisplay = (optionsFromArguments.attributesToDisplay || '')
+    .split(',')
+    .filter(Boolean)
+    .map(x => x.trim());
+
+  try {
+    checkAppPath(applicationPath);
+    checkAppName(appName);
+  } catch (err) {
+    console.error(err.message);
+    console.log();
+
+    process.exit(1);
+  }
+
+  const questions = [
+    {
+      type: 'list',
+      pageSize: 10,
+      name: 'template',
+      message: 'InstantSearch template',
+      choices: () => {
+        const templatesByCategory = getTemplatesByCategory();
+
+        return Object.entries(templatesByCategory).reduce(
+          (templates, [category, values]) => [
+            ...templates,
+            new inquirer.Separator(category),
+            ...values,
+          ],
+          []
+        );
+      },
+      validate(input) {
+        return Boolean(input);
+      },
     },
-  },
-  {
-    type: 'list',
-    name: 'libraryVersion',
-    message: answers => `${answers.template} version`,
-    choices: async answers => {
-      const templatePath = getTemplatePath(answers.template);
-      const templateConfig = getAppTemplateConfig(templatePath);
-      const { libraryName } = templateConfig;
+    {
+      type: 'list',
+      name: 'libraryVersion',
+      message: answers => `${answers.template} version`,
+      choices: async answers => {
+        const templatePath = getTemplatePath(answers.template);
+        const templateConfig = getAppTemplateConfig(templatePath);
+        const { libraryName } = templateConfig;
 
-      try {
-        const versions = await fetchLibraryVersions(libraryName);
-        const latestStableVersion = latestSemver(versions);
+        try {
+          const versions = await fetchLibraryVersions(libraryName);
+          const latestStableVersion = latestSemver(versions);
 
-        if (!latestStableVersion) {
-          return versions;
+          if (!latestStableVersion) {
+            return versions;
+          }
+
+          return [
+            new inquirer.Separator('Latest stable version (recommended)'),
+            latestStableVersion,
+            new inquirer.Separator('All versions'),
+            ...versions,
+          ];
+        } catch (err) {
+          const fallbackLibraryVersion = '1.0.0';
+
+          console.log();
+          console.error(
+            chalk.red(
+              `Cannot fetch versions for library "${chalk.cyan(libraryName)}".`
+            )
+          );
+          console.log();
+          console.log(
+            `Fallback to ${chalk.cyan(
+              fallbackLibraryVersion
+            )}, please upgrade the dependency after generating the app.`
+          );
+          console.log();
+
+          return [
+            new inquirer.Separator('Available versions'),
+            fallbackLibraryVersion,
+          ];
         }
-
-        return [
-          new inquirer.Separator('Latest stable version (recommended)'),
-          latestStableVersion,
-          new inquirer.Separator('All versions'),
-          ...versions,
-        ];
-      } catch (err) {
-        const fallbackLibraryVersion = '1.0.0';
-
-        console.log();
-        console.error(
-          chalk.red(
-            `Cannot fetch versions for library "${chalk.cyan(libraryName)}".`
-          )
-        );
-        console.log();
-        console.log(
-          `Fallback to ${chalk.cyan(
-            fallbackLibraryVersion
-          )}, please upgrade the dependency after generating the app.`
-        );
-        console.log();
-
-        return [
-          new inquirer.Separator('Available versions'),
-          fallbackLibraryVersion,
-        ];
-      }
-    },
-    when: answers => {
-      const templatePath = getTemplatePath(answers.template);
-      const templateConfig = getAppTemplateConfig(templatePath);
-
-      return Boolean(templateConfig.libraryName);
-    },
-  },
-  {
-    type: 'input',
-    name: 'appId',
-    message: 'Application ID',
-  },
-  {
-    type: 'input',
-    name: 'apiKey',
-    message: 'Search API key',
-  },
-  {
-    type: 'input',
-    name: 'indexName',
-    message: 'Index name',
-  },
-  {
-    type: 'checkbox',
-    name: 'attributesToDisplay',
-    message: 'Attributes to display',
-    suffix: `\n  ${chalk.gray('Used to generate the default result template')}`,
-    pageSize: 10,
-    choices: async answers => [
-      {
-        name: 'None',
-        value: undefined,
       },
-      new inquirer.Separator(),
-      new inquirer.Separator('From your index'),
-      ...(await getAttributesFromIndex(answers)),
-    ],
-    filter: attributes => attributes.filter(Boolean),
-    when: ({ appId, apiKey, indexName }) =>
-      !attributesToDisplay.length > 0 && appId && apiKey && indexName,
-  },
-].filter(question => isQuestionAsked({ question, args: optionsFromArguments }));
+      when: answers => {
+        const templatePath = getTemplatePath(answers.template);
+        const templateConfig = getAppTemplateConfig(templatePath);
 
-async function run() {
-  console.log();
-  console.log(`Creating a new InstantSearch app in ${chalk.green(appPath)}.`);
-  console.log();
-
-  const config = {
-    ...(await getConfiguration({
-      options: {
-        ...optionsFromArguments,
-        name: appName,
-        attributesToDisplay,
+        return Boolean(templateConfig.libraryName);
       },
-      answers: await inquirer.prompt(questions),
-    })),
-    installation: program.installation,
-  };
+    },
+    {
+      type: 'input',
+      name: 'appId',
+      message: 'Application ID',
+    },
+    {
+      type: 'input',
+      name: 'apiKey',
+      message: 'Search API key',
+    },
+    {
+      type: 'input',
+      name: 'indexName',
+      message: 'Index name',
+    },
+    {
+      type: 'checkbox',
+      name: 'attributesToDisplay',
+      message: 'Attributes to display',
+      suffix: `\n  ${chalk.gray(
+        'Used to generate the default result template'
+      )}`,
+      pageSize: 10,
+      choices: async answers => [
+        {
+          name: 'None',
+          value: undefined,
+        },
+        new inquirer.Separator(),
+        new inquirer.Separator('From your index'),
+        ...(await getAttributesFromIndex(answers)),
+      ],
+      filter: attributes => attributes.filter(Boolean),
+      when: ({ appId, apiKey, indexName }) =>
+        !attributesToDisplay.length > 0 && appId && apiKey && indexName,
+    },
+  ].filter(question =>
+    isQuestionAsked({ question, args: optionsFromArguments })
+  );
 
-  const templatePath = getTemplatePath(config.template);
-  const { tasks } = getAppTemplateConfig(templatePath);
-  const app = createInstantSearchApp(appPath, config, tasks);
+  async function run() {
+    console.log();
+    console.log(
+      `Creating a new InstantSearch app in ${chalk.green(applicationPath)}.`
+    );
+    console.log();
 
-  await app.create();
-}
+    const config = {
+      ...(await getConfiguration({
+        options: {
+          ...optionsFromArguments,
+          name: appName,
+          attributesToDisplay,
+        },
+        answers: await inquirer.prompt(questions),
+      })),
+      installation: program.installation,
+    };
 
-run().catch(err => {
-  console.error(err.message);
-  console.log();
+    const templatePath = getTemplatePath(config.template);
+    const { tasks } = getAppTemplateConfig(templatePath);
+    const app = createInstantSearchApp(applicationPath, config, tasks);
 
-  process.exit(2);
-});
+    await app.create();
+  }
 
-process.on('SIGINT', () => {
-  process.exit(3);
+  run().catch(err => {
+    console.error(err.message);
+    console.log();
+
+    process.exit(2);
+  });
+
+  process.on('SIGINT', () => {
+    process.exit(3);
+  });
 });
