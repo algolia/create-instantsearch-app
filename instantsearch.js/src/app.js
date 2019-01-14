8d6e75049b5d92bce9b91d74babdc989ae0e63de
François Chalifour <francois.chalifour@gmail.com>
2019-01-14 10:22:28 +0100

    feat(template): Update templates

--- instantsearch.js/src/app.js
+++ instantsearch.js/src/app.js
@@ -1,15 +1,18 @@
-/* global instantsearch */
+/* global algoliasearch instantsearch */
+
+const searchClient = algoliasearch(
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+);
 
 const search = instantsearch({
-  appId: 'latency',
-  apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
   indexName: 'instant_search',
+  searchClient,
 });
 
 search.addWidget(
   instantsearch.widgets.searchBox({
     container: '#searchbox',
-    autofocus: false,
   })
 );
 
@@ -19,8 +22,8 @@ search.addWidget(
     templates: {
       item: `
         <article>
-          <h1>{{{_highlightResult.name.value}}}</h1>
-          <p>{{{_highlightResult.description.value}}}</p>
+          <h1>{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}</h1>
+          <p>{{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}</p>
         </article>
       `,
     },
@@ -30,7 +33,7 @@ search.addWidget(
 search.addWidget(
   instantsearch.widgets.refinementList({
     container: '#brand-list',
-    attributeName: 'brand',
+    attribute: 'brand',
   })
 );
 
