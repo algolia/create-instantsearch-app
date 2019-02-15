01ed0c30bea6261b9b8f324d9ef6960afd96c656
François Chalifour <francois.chalifour@gmail.com>
2019-02-15 10:46:09 +0100

    fix(api): resolve relative paths

--- src/api/check-config.js
+++ src/api/check-config.js
@@ -17,10 +17,7 @@ function getOptions({ supportedTemplates }) {
     },
     template: {
       validate(input) {
-        return (
-          supportedTemplates.includes(input) ||
-          fs.existsSync(`${input}/.template.js`)
-        );
+        return fs.existsSync(`${input}/.template.js`);
       },
       getErrorMessage() {
         return `The template directory must contain a configuration file \`.template.js\` or must be one of those: ${supportedTemplates.join(
