b541974ecb3d63c5e90eab434da5535b74dab322
eunjae-lee <karis612@gmail.com>
2021-05-28 14:42:44 +0200

    chore: remove space from templateName

--- src/templates/React InstantSearch widget/.template.js
+++ src/templates/React InstantSearch widget/.template.js
@@ -5,7 +5,7 @@ module.exports = {
   category: 'Widget',
   libraryName: 'react-instantsearch',
   supportedVersion: '>= 6.0.0 < 7.0.0',
-  templateName: 'react-instantsearch widget',
+  templateName: 'react-instantsearch-widget',
   appName: 'react-instantsearch-app',
   keywords: [
     'algolia',
