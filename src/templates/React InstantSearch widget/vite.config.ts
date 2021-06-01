9856a5b7938e2ffc5787f86715714ff42e881a55
eunjae-lee <karis612@gmail.com>
2021-06-01 17:08:38 +0200

    specify ReactDOM as globals

--- src/templates/React InstantSearch widget/vite.config.ts
+++ src/templates/React InstantSearch widget/vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
       output: {
         globals: {
           react: 'React',
+          'react-dom': 'ReactDOM',
           'react-instantsearch-dom': 'ReactInstantSearchDOM',
         },
       },
